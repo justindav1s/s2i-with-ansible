b64f2304f7b7eaedd925e608c9a52856d40f9f60
justindav1s <justin.davis@ba.com>
2016-11-12 20:31:48 +0000

    new playbook

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export ANSIBLE_ROOT=/tmp/src/ansible
+
 echo This is deploy.sh
 
 echo I am `whoami`
@@ -7,3 +9,5 @@ echo I am `whoami`
 echo `pwd`
 
 ansible localhost -m shell -a 'uname -a'
+
+ansible-playbook -vv ${ANSIBLE_ROOT}/playbooks/check.yml
---
+++ webapps/s2itest/index.jsp
@@ -0,0 +1,3 @@
+<h3> This is is the s2itest index.jsp</h3>
+<br/>
+It was deployed as pasrt of an s2i build using Ansible.
